9a2ec0c44bdf0396a00a124a851c20ab3fbd7aa5
Sharad Kumar Singh <singh.sharadk@gmail.com>
2020-11-26 08:54:56 +0530

    new features

--- mail_gmail.php
+++ mail_gmail.php
@@ -1,59 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<!--<div class="container"> -->
+
+<nav class="navbar navbar-dark fixed-top bg-warning">
+ <a class="navbar-brand" href="srm_vendor_main.php"><img src="sail-logo.jpg" width="40" height="40" alt="BGH-MAIN"></a> 
+  <h6>SRM-Vendor Initial Activation Mail Send For a Specific Date</h6>
+<div class="container">
+<form  class="form-inline" name="myform" action="mail_gmail.php" method="POST"> <input type="hidden" name="check_submit" value="1" />     
+  
+<div class="form-group">  
+        <label for="stdate">Received From ERP From Date</label>  
+        <input class="form-control mr-sm-2" type="date"   id="stdate" name="stdate" placeholder="Date" aria-label="stdate" value="<?php echo isset($_POST['stdate']) ? $_POST['stdate']:''; ?>">
+    </div>     
+         
+         
+    <div class="form-group">  
+        <label for="endate">To Date</label>  
+        <input class="form-control mr-sm-2" type="date" id="endate" name="endate" placeholder="ToDate"   aria-label="todate" value="<?php echo isset($_POST['endate']) ? $_POST['endate']:''; ?>">
+    </div>  
+          
+    <button class="btn btn-success my-2 my-sm-0" type="submit" name="getdata">Get Data...</button>
+    <button class="btn btn-warning my-2 my-sm-0" type="submit" name="sendmail">Send Mail...</button>
+
+    
+</form>
+</div>
+</nav>
+
 <?php
-
-//require_once '..\vendor\autoload.php';
-//require('.\fpdf182\fpdf.php');
-
-//require('..\vendor\phpmailer\phpmailer\src\PHPMailer.php');
-//require('..\vendor\phpmailer\phpmailer\src\SMTP.php');
-//require('..\vendor\phpmailer\phpmailer\src\Exception.php');
-
-require('.\phpmailer\phpmailer\src\PHPMailer.php');
-require('.\phpmailer\phpmailer\src\SMTP.php');
-require('.\phpmailer\phpmailer\src\Exception.php');
-
-
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-//$mail->IsSMTP(); 
-
-
-//$mail=new PHPMailer;
-
-//Enable SMTP debugging. 
-$mail->SMTPDebug = 3;                               
-//Set PHPMailer to use SMTP.
-$mail->isSMTP();            
-//Set SMTP host name                          
-$mail->Host = "smtp.gmail.com";
-//Set this to true if SMTP host requires authentication to send email
-$mail->SMTPAuth = true;                          
-//Provide username and password     
-$mail->Username = "citbgh@gmail.com";                 
-$mail->Password = "citbgh@827001";                           
-//If SMTP requires TLS encryption then set it
-$mail->SMTPSecure = "tls";                           
-//Set TCP port to connect to 
-$mail->Port = 587;                                   
-
-//username = 'citbgh@gmail.com'
-//password = 'citbgh@827001'
-
-
-$mail->From = "citbgh@gmail.com.com";
-$mail->FromName = "Computer and Information Technology BGH";
-
-$mail->addAddress("singh.sharadk@gmail.com", "Recepient Name");
-
-$mail->isHTML(true);
-
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
-
-if(!$mail->send()) 
+//if (array_key_exists('check_submit', $_POST)) 
+if (isset($_POST['getdata'])) 
 {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} 
-else 
-{
-    echo "Message has been sent successfully";
+
+  if (isset($_POST['stdate'])){$stdate    =  $_POST['stdate'];}
+  if (isset($_POST['endate'])){$endate    =  $_POST['endate'];}
+
+
+    require('.\phpmailer\phpmailer\src\PHPMailer.php');
+    require('.\phpmailer\phpmailer\src\SMTP.php');
+    require('.\phpmailer\phpmailer\src\Exception.php');
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    //$mail->IsSMTP(); 
+
+    //$mail=new PHPMailer;
+
+    //Enable SMTP debugging. 
+    $mail->SMTPDebug = 0;                               
+    //Set PHPMailer to use SMTP.
+    $mail->isSMTP();            
+    //Set SMTP host name                          
+    $mail->Host = "smtp.gmail.com";
+    //Set this to true if SMTP host requires authentication to send email
+    $mail->SMTPAuth = true;                          
+    //Provide username and password     
+    $mail->Username = "sailbslsrm@gmail.com";                 
+    $mail->Password = "sk940678";                           
+    //If SMTP requires TLS encryption then set it
+    $mail->SMTPSecure = "tls";                           
+    //Set TCP port to connect to 
+    $mail->Port = 587;                                   
+
+    //username = 'citbgh@gmail.com'
+    //password = 'citbgh@827001'
+
+
+    $mail->From = "sailbslsrm@gmail.com";
+    $mail->FromName = "SAIL: Bokaro Steel Plant";
+    $mail->addReplyTo('bsl.srm@sail.in', 'bsl srm');
+    $mail->isHTML(false);
+    $mail->Subject = "SAIL: Bokro Steel Plant: SRM Credentials";
+    $mail->AltBody = "This is the plain text version of the email content";
+
+
+    $conn = oci_connect("bgh", "hpv185e", "10.143.100.36/BGH6");
+    if (!$conn) {
+        $e = oci_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+        $stid = oci_parse($conn, "SELECT v_code, v_name, v_email, init_pass 
+        FROM srm_mail where to_char(recd_from_pur,'YYYY-MM-DD') between :EIDBV and :EIDBV1");
+
+        $mystdate = $stdate;
+        oci_bind_by_name($stid, ":EIDBV", $mystdate);
+
+        $myendate = $endate;
+        oci_bind_by_name($stid, ":EIDBV1", $myendate);
+
+        oci_execute($stid);
+
+while (($row = oci_fetch_row($stid)) != false) {
+//    echo $row[0] . " " . $row[1] . "<br>\n";
+
+//    $mail->addAddress("singh.sharadk@gmail.com", "Recepient Name");
+
+    $mail->addAddress("$row[2]", "Recepient Name");
+
+        $mail->Body = "Dear Sir/Madam". "\n".
+                  "your credentials are". "\n" .
+                  "Userid:". $row[0]. "\n" .
+                  "Initial Password:".$row[3]. "\n".
+                  "Please Change on First Log On". "\n".
+                  ".........BSL/SRM";
+
+    if(!$mail->send()) 
+    {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } 
+        else 
+    {
+        echo "Message has been sent successfully";
+        $stid = oci_parse($conn, "update srm_mail set mail_sent_date=trunc(sysdate), 
+        pass_sent_date=trunc(sysdate), mail_recd_date=trunc(sysdate) 
+        where v_code=". "'". $row[0]. "'");
+        oci_execute($stid);
+
+
+
+    }
 }
+
+oci_free_statement($stid);
+oci_close($conn);
+}
+else if (isset($_POST['sendmail']))
+{
+    
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+    echo "<h1>PHP is Fun</h1>";
+
+
+}
+
+?>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
